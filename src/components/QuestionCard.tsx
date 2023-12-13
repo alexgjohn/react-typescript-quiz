a064baa11c3a69d443f869448b783e044078f930
Alex <alexgjohn@outlook.com>
2023-12-13 10:48:44 +0000

    implemented functionality of quiz game, but still need styling and some other ux elements like displaying corrrect answer after each question

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -25,9 +25,10 @@ const QuestionCard: React.FC<Props> = ({
             <p dangerouslySetInnerHTML={{ __html: question}}/>
             <div>
                 {answers.map(answer => (
-                    <div>
+                    <div key={answer}>
                         <button 
                             disabled={userAnswer} 
+                            value={answer}
                             onClick={callback}>
                             <span dangerouslySetInnerHTML={{ __html: answer}} />
                         </button>
